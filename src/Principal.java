7ac9682ce80614be6e695ba4d0b31f4e6a7d9511
Venom <rafael.santos.garcia@hotmail.com>
2022-01-16 12:42:20 -0300

    Refatorando main

--- src/Principal.java
+++ src/Principal.java
@@ -2,42 +2,48 @@ import java.io.FileNotFoundException;
 
 public class Principal {
 
+	public static BancoDePalavras bp;
+	public static IO io;
+	public static MecanicaDeJogo mj;
+	
 	public static void main(String[] args) {
 		try {
-			BancoDePalavras bp = new BancoDePalavras();
-			try {
-				
-				IO io = new IO();
-				MecanicaDeJogo mj = FactoryMecanicaDeJogo.factory(io.mecanicaDeJogo());
-				
-				do {
-					//Inicialização da Rodada
-					mj.setEmbaralhador(new EmbaralhadorOrdemAlfabetica());
-					
-					try {
-						mj.setPalavra(bp.getPalavra());
-					}
-					catch(Exception e){
-						IO.imprimeException(e);
-					}
-					
-					IO.apresentaPalavra(mj.getPalavra());
-					while(mj.novaTentativa()) {
-						mj.tentativa(io.entrada());
-					}
-					
-				}while(!mj.fimDeJogo());
-				
-				io.close();
-			}
-			
-			catch(IllegalArgumentException iae){
-				IO.imprimeException(iae);
-			}
+			bp = new BancoDePalavras();	
 		}
 		catch(FileNotFoundException fnfe){
 			IO.imprimeException(fnfe);
+			return;
 		}
+		
+		try {
+			io = new IO();
+			mj = FactoryMecanicaDeJogo.factory(io.mecanicaDeJogo());
+		}
+		catch(IllegalArgumentException iae){
+			IO.imprimeException(iae);
+			return;
+		}
+		
+		do {
+			//Inicialização da Rodada
+			mj.setEmbaralhador(new EmbaralhadorOrdemAlfabetica());
+			
+			try {
+				mj.setPalavra(bp.getPalavra());
+			}
+			catch(Exception e){
+				IO.imprimeException(e);
+				return;
+			}
+			
+			IO.apresentaPalavra(mj.getPalavra());
+			while(mj.novaTentativa()) {
+				mj.tentativa(io.entrada());
+			}
+			
+		}while(!mj.fimDeJogo());
+		
+		io.close();
 	}
 
 }
