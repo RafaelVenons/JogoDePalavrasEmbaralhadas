bf5da0fb7911e31b5310089a7e2c57b0fe019121
Venom <rafael.santos.garcia@hotmail.com>
2022-01-16 11:24:50 -0300

    Refatorando Mecanica de Jogo de Palavra Unica

--- src/BancoDePalavras.java
+++ src/BancoDePalavras.java
@@ -10,7 +10,7 @@ public class BancoDePalavras {
 	
 	public BancoDePalavras() throws FileNotFoundException{
 		
-		Scanner in = new Scanner(new FileReader("src\\dads.txt"));
+		Scanner in = new Scanner(new FileReader("src\\dados.txt"));
 		
 		this.lista = new Vector<String>();
 		
